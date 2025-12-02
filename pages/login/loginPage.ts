52281ef5c2310943544f449d076b68b958c40b71
unknown <bigyama256@gmail.com>
2025-12-02 13:52:29 +0545

    login page test scripts

---
+++ pages/login/loginPage.ts
@@ -0,0 +1,107 @@
+import { BasePage } from "../basePage";
+import { Locator, Page, expect } from "@playwright/test";
+
+export class LoginPage extends BasePage {
+  readonly logo: Locator;
+  readonly headingTitle: Locator;
+  readonly subTitle: Locator;
+  readonly emailField: Locator;
+  readonly passwordField: Locator;
+  readonly rememberMeCheckbox: Locator;
+  readonly forgotPasswordLink: Locator;
+  readonly loginButton: Locator;
+  readonly emailError: Locator;
+  readonly passwordError: Locator;
+  readonly clickHereLink: Locator;
+  readonly togglePasswordIcon: Locator;
+
+  constructor(page: Page) {
+    super(page);
+
+    this.logo = page.locator("#bodyContent").getByRole("img");
+    this.headingTitle = page.getByRole("heading", {
+      name: "Welcome to Coalition Manager",
+    });
+    this.subTitle = page.getByText("Login to your account");
+    this.emailField = page.getByRole("textbox", { name: "Email" });
+    this.passwordField = page.getByRole("textbox", { name: "Password" });
+    this.rememberMeCheckbox = page.getByRole("checkbox", {
+      name: "Remember me",
+    });
+    this.forgotPasswordLink = page.getByRole("link", {
+      name: "Forgot Password?",
+    });
+    this.loginButton = page.getByRole("button", { name: "Login" });
+    this.emailError = page.locator('span[data-valmsg-for="Email"]');
+    this.passwordError = page.locator('span[data-valmsg-for="Password"]');
+    this.clickHereLink = page.getByRole("link", { name: "Click here" });
+    this.togglePasswordIcon = page.locator(".toggle-password").first();
+  }
+
+  async login(email: string, password: string) {
+    await this.type(this.emailField, email);
+    await this.type(this.passwordField, password);
+    await this.click(this.loginButton);
+  }
+
+  async clearAndClickLogin() {
+    await this.click(this.emailField);
+    await this.emailField.clear();
+    await this.click(this.passwordField);
+    await this.passwordField.clear();
+    await this.click(this.loginButton);
+  }
+
+  async toggleRememberMe() {
+    await this.click(this.rememberMeCheckbox);
+  }
+
+  async goToForgotPasswordPage() {
+    await this.click(this.forgotPasswordLink);
+  }
+
+  async goToRegisterPage() {
+    await this.click(this.clickHereLink);
+  }
+
+  async validateLoginPageElements() {
+    await expect(this.logo).toBeVisible();
+    await expect(this.headingTitle).toContainText(
+      "Welcome to Coalition Manager"
+    );
+    await expect(this.subTitle).toBeVisible();
+    await expect(this.emailField).toBeVisible();
+    await expect(this.passwordField).toBeVisible();
+    await expect(this.rememberMeCheckbox).toBeVisible();
+    await expect(this.forgotPasswordLink).toBeVisible();
+    await expect(this.loginButton).toBeVisible();
+  }
+
+  async validateRequiredErrors() {
+    await expect(this.emailError).toHaveText("The Email field is required.");
+    await expect(this.passwordError).toHaveText(
+      "The Password field is required."
+    );
+  }
+
+  async validateInvalidEmailError() {
+    await expect(this.emailError).toHaveText(
+      "The Email field is not a valid e-mail address."
+    );
+  }
+
+  async togglePasswordHideIcon() {
+    await expect(this.togglePasswordIcon).toBeVisible();
+    await expect(this.togglePasswordIcon).toHaveClass(/fa-eye/);
+  }
+
+  async togglePasswordShowIcon() {
+    await expect(this.togglePasswordIcon).toBeVisible();
+    await expect(this.togglePasswordIcon).toHaveClass(/fa-eye-slash/);
+  }
+
+   async clickTogglePassword() {
+    await expect(this.togglePasswordIcon).toBeVisible();
+    await this.togglePasswordIcon.click();
+  }
+}
---
+++ specfiles/login/loginPage.spec.ts
@@ -0,0 +1,88 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../../pages/login/loginPage";
+import credentials from "../../fixtures/common/adminlogindata.json";
+
+test.describe("Login Page : Coalition Manager", () => {
+  test.beforeEach(async ({ page }, testInfo) => {
+    const login = new LoginPage(page);
+    await login.navigate(testInfo.project.use.baseURL!);
+  });
+
+  test("Verify the visibility of UI elements on login page", async ({
+    page,
+  }) => {
+    const login = new LoginPage(page);
+    await login.validateLoginPageElements();
+  });
+
+  test("Try logging in with empty fields and validating the display of validation messages for email and password fields. ", async ({
+    page,
+  }) => {
+    const login = new LoginPage(page);
+    await login.clearAndClickLogin();
+    await login.validateRequiredErrors();
+  });
+
+  test("Verfiy by logging in with invalid username & password", async ({
+    page,
+  }) => {
+    const login = new LoginPage(page);
+    const user = credentials.invalidUser;
+    await login.login(user.username, user.password);
+    await expect(page.getByText("Invalid login attempt")).toBeVisible();
+  });
+
+  test("Verfiy by logging in with invalid email in username .", async ({
+    page,
+  }) => {
+    const login = new LoginPage(page);
+    const user = credentials.invalidEmailUsername;
+    await login.login(user.username, user.password);
+    await login.validateInvalidEmailError();
+  });
+
+  test("Verify password show/hide icon works correctly", async ({ page }) => {
+    const login = new LoginPage(page);
+    await login.passwordField.fill('TestPassword123');
+    await login.togglePasswordHideIcon();
+
+    await login.clickTogglePassword();
+    await login.togglePasswordShowIcon();
+
+    await login.clickTogglePassword();
+    await login.togglePasswordHideIcon();
+  });
+
+  test("Verify by checking and unchecking remember me checkbox toggle", async ({
+    page,
+  }) => {
+    const login = new LoginPage(page);
+    await login.toggleRememberMe();
+    await expect(login.rememberMeCheckbox).toBeChecked();
+    await login.toggleRememberMe();
+    await expect(login.rememberMeCheckbox).not.toBeChecked();
+  });
+
+  test("Verify the Forgot Password flow by ensuring the reset link opens the proper page.", async ({
+    page,
+  }) => {
+    const login = new LoginPage(page);
+    await login.goToForgotPasswordPage();
+    await expect(page).toHaveURL(/ForgotPassword/);
+  });
+
+  test("Verify the Registration link redirects correctly", async ({ page }) => {
+    const login = new LoginPage(page);
+    await login.goToRegisterPage();
+    await expect(page).toHaveURL(
+      "https://staging2.coalitionmanager.org/staging-combined/contactmanager/contact/publicregistration"
+    );
+  });
+
+  test("Verify successful login with correct credentials", async ({ page }) => {
+    const login = new LoginPage(page);
+    const user = credentials.validUser;
+    await login.login(user.username, user.password);
+  });
+  
+});
